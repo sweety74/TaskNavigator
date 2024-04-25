e63ea4747c24705fbf882457e757631d1c42814b
sweety74 <sweetysolanki74@gmail.com>
2024-04-25 13:08:32 +0530

    imported routes to app.js

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const app = express();
-
+const task = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
 
-app.get('/',(req,res)=>{
-    res.send('hello');
-})
+
 //this middleware function is required to be used before the routes so that we can 
 //use json middleware to parse the json data from api's and req body.
 app.use(express.json());
-
+app.use('/api/v1/tasks',task);
 
 
 const port = 3000;
