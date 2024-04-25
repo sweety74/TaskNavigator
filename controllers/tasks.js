f85ddd7f02802dd4815d693ab4b1efbf80703e4f
sweety74 <sweetysolanki74@gmail.com>
2024-04-25 13:04:35 +0530

    created controllers

---
+++ controllers/tasks.js
@@ -0,0 +1,79 @@
+const Task = require('../models/task');
+
+
+const getAllTasks =async  (req,res) =>{
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks })
+  } catch (error) {
+    res.status(500).json({msg:error});
+  }
+}
+
+const createTask = async (req,res) =>{
+  try {
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
+
+  } catch (error) {
+    res.status(500).json({msg:error});
+  }
+
+
+}
+
+const getTask = async (req,res) =>{
+  try {
+    const {id:TaskID} = req.params
+    const task = await Task.findById(TaskID);
+    if(!task){
+      return res.status(404).json({msg:`No tak with id :${TaskID}`})
+    }
+
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({msg:error});
+  }
+}
+
+const updateTask = async (req,res) =>{
+  try {
+    const {id:taskID} = req.params;
+    
+    const task = await Task.findOneAndUpdate({_id:taskID},req.body,{
+      new:true,
+      runValidators:true
+    })
+    if(!task){
+      return res.status(404).json({msg:`No tak with id :${taskID}`})
+    }
+
+    res.status(200).json({ task})
+
+  } catch (error) {
+    res.status(500).json({msg:error});
+  }
+}
+
+const deleteTask = async (req,res) =>{
+  try {
+    const {id:taskID} = req.params;
+    const task = await Task.findByIdAndDelete(taskID);
+    
+    if(!task){
+      return res.status(404).json({msg:`No tak with id :${taskID}`})
+    }
+    res.status(200).json({task});
+
+  } catch (error) {
+    res.status(500).json({msg:error});
+  }
+}
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask
+}
